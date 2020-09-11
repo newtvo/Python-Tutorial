152091e7463a2e4de7f32048d55afb4abec9b0c8
Newt vo <nhutmvo1997@gmail.com>
2020-09-10 23:58:18 -0400

    Comment Tut

--- variables/var.py
+++ variables/var.py
@@ -1,12 +1,16 @@
+# This is how we can create a comment
+# Using ''' ''' to create multiple comment line
 x=5
 print(type(x))
 x='teo'
 print(type(x))
 x=True
 print(type(x))
+'''
 x=5.5
 print(type(x))
 x=complex(21,12)
 print(type(x))
 x='Obama! Happy New Year!'
 print(type(x))
+'''
