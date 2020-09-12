d08efd3b3c96842423d7451e8d190cc5db34456d
Newt vo <nhutmvo1997@gmail.com>
2020-09-12 17:33:07 -0400

    Errors

---
+++ Errors/logic-error.py
@@ -0,0 +1,6 @@
+t = 1
+l = 9
+h = 3
+# Wrong logic, should be (t+l+h)/3
+dtb = t + l + h/3
+print(dtb)
---
+++ Errors/runtime-exception.py
@@ -0,0 +1,10 @@
+try:
+    x = 5
+    y = 0
+    z = x / y
+    print(z)
+except:
+    print('There is an error!')
+
+print('Thank you for using the program!')
+
