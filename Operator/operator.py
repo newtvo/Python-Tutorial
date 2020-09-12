d2d72fc178a665d45e4d32a745157447b5655051
Newt vo <nhutmvo1997@gmail.com>
2020-09-12 16:37:25 -0400

    Operator in Python

---
+++ Operator/operator.py
@@ -0,0 +1,18 @@
+x=5
+y=x**2
+print(x)
+print(y)
+
+year = 2020
+t = year % 2 == 0 or year % 3 > 1
+print(t)
+
+print("*"*20)
+print(5 is 5)
+print(5 is not 5)
+
+print("*"*20)
+print(9/2)
+print(9//2)
+
+
